80191696c484a7b09f503b185ddda74eb826cd06
Jose Lobato <joselobatof@gmail.com>
2021-09-25 19:44:51 -0500

    POJO with application.properties

--- fundamentos/src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ fundamentos/src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -2,6 +2,7 @@ package com.fundamentos.springboot.fundamentos;
 
 import com.fundamentos.springboot.fundamentos.bean.MyBean;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -14,11 +15,18 @@ public class FundamentosApplication implements CommandLineRunner {
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
+	private MyBeanWithProperties myBeanWithProperties;
 
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency){
+	public FundamentosApplication(
+			@Qualifier("componentTwoImplement") ComponentDependency componentDependency,
+			MyBean myBean,
+			MyBeanWithDependency myBeanWithDependency,
+			MyBeanWithProperties myBeanWithProperties
+	){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
+		this.myBeanWithProperties = myBeanWithProperties;
 	}
 
 	public static void main(String[] args) {
@@ -30,5 +38,6 @@ public class FundamentosApplication implements CommandLineRunner {
 		componentDependency.saludar();
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
+		System.out.println(myBeanWithProperties.function());
 	}
 }
